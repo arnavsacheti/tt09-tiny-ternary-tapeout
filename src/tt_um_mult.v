931ef2895208ceae60d45146b54ed684c4bbb303
Arnav Sacheti <36746504+arnavsacheti@users.noreply.github.com>
2024-10-20 18:01:16 +0000

    One Tile - 90.317%

--- src/tt_um_mult.v
+++ src/tt_um_mult.v
@@ -21,24 +21,25 @@ module tt_um_mult # (
    reg [BitWidth*OutLen-1:0]             pipe_out;
    integer                               col;
 
-   wire [2*OutLen-1:0] row_data1 = W[({24'b0, row, 5'b0_0000})+: 2*OutLen]; // Register to hold the entire row
-   wire [2*OutLen-1:0] row_data2 = W[({24'b0, row, 5'b1_0000})+: 2*OutLen]; // Register to hold the entire row
+   wire [2*OutLen-1:0] row_data1 = W[{row, 1'b0, 4'h0} +: 2*OutLen]; // Register to hold the entire row
+   wire [2*OutLen-1:0] row_data2 = W[{row, 1'b1, 4'h0} +: 2*OutLen]; // Register to hold the entire row
 
    always @(posedge clk) begin
       // Logic for computing the temporary sums (before piping into registers)
-      for (col = 0; col < OutLen*2; col = col + 2) begin
+      
+      for (col = 0; col < InLen; col = col + 1) begin
             // If we are not at the end of the loop
             // Update temp_out based on current W values
-            temp_out[(col<<2)+:BitWidth] <= (row_data1[(col)+:2] == 2'b11 ? (-$signed(VecIn[BitWidth+:BitWidth])) :
-                                             row_data1[(col)+:2] == 2'b01 ? $signed(VecIn[BitWidth+:BitWidth]) : {BitWidth{1'b0}}) +
-                                            (row_data2[(col)+:2] == 2'b11 ? (-$signed(VecIn[0+:BitWidth])) :
-                                             row_data2[(col)+:2] == 2'b01 ? $signed(VecIn[0+:BitWidth]) : {BitWidth{1'b0}}) +
-                                            (row == 3'b0 ? {BitWidth{1'b0}} : $signed(temp_out[col<<2+:BitWidth]));
+            temp_out[(col<<3)+:BitWidth] <= (row_data1[col[3:0] + 1'b1] ? (-$signed(VecIn[BitWidth+:BitWidth])) :
+                                             row_data1[col[3:0]] ? $signed(VecIn[BitWidth+:BitWidth]) : {BitWidth{1'b0}}) +
+                                            (row_data2[col[3:0] + 1'b1] ? (-$signed(VecIn[0+:BitWidth])) :
+                                             row_data2[col[3:0]] ? $signed(VecIn[0+:BitWidth]) : {BitWidth{1'b0}}) +
+                                            (|row ? $signed(temp_out[col<<2+:BitWidth]) : {BitWidth{1'b0}});
       end
    end
 
    always @(row) begin
-      if(row==3'b0 && en) 
+      if(~|row && en) 
          pipe_out = temp_out;
   end
 
