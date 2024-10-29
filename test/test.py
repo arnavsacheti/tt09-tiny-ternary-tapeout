300fba87e9c7feec6d65ec8a1e1cade7fd090631
jackadiletta <jta2021@outlook.com>
2024-10-29 22:16:19 +0000

    More thorough test bench

--- test/test.py
+++ test/test.py
@@ -31,9 +31,34 @@ async def test_project(dut) -> None:
     dut._log.info("Test project behavior")
 
     # Set the input values you want to test
-    # Known Values test
     weights = Weights(dut)
     values = [-1, 0, 1] # 
+    w = np.random.choice(values, size=(14, 7)).tolist()
+    await weights.set_weights(w)
+    
+
+
+    vecs = Vecs(dut, w)
+    await vecs.drive_vecs(runs=15)
+
+    # test reset
+
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 1)
+    dut.rst_n.value = 1
+    await weights.set_weights(w)
+
+    await vecs.drive_vecs(runs=15)
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 1)
+    assert weights.check_weights()
+
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 1
+    dut._log.info("Test project behavior")
+
+    # Set the input values you want to test
     w = np.random.choice(values, size=(14, 7)).tolist()
     await weights.set_weights(w)
     
@@ -50,8 +75,3 @@ async def test_project(dut) -> None:
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 1)
     assert weights.check_weights()
-    dut.rst_n.value = 1
-    await ClockCycles(dut.clk, 1)
-    dut.ui_in.value  = 0x00
-    dut.uio_in.value = 0x00
-    await ClockCycles(dut.clk, 1)
